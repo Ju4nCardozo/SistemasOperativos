c15d9cdf57b97fd5a59f9da1b9d63f423a7f6dcd
Juan Cardozo <juan.cardozog@upb.edu.co>
2021-02-11 20:32:07 -0500

    Desarrollo punto 11 del ejercicio 6 de la unidad 1

--- Unidad_1/Ejercicio_1_6_11/src/Ejercicio_1_6_11.c
+++ Unidad_1/Ejercicio_1_6_11/src/Ejercicio_1_6_11.c
@@ -9,8 +9,35 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+float promedio(float A[]);
+float float_rand( float min, float max );
 
 int main(void) {
 
+	float A[10];
+	for(int i=0; i<=9;i++){
+		A[i] = float_rand(0,100);
+	}
+	promedio(A);
 	return 0;
+}
+
+float promedio(float A[]){
+
+	float promedio;
+	float suma;
+	for(int i=0; i<=9;i++){
+		suma += A[i];
+	}
+	promedio = suma/10;
+	printf("promedio es: %f\n", promedio);
+}
+
+float float_rand( float min, float max )
+{
+    srand ( time(NULL) );
+    float scale = rand() / (float) RAND_MAX;
+    return min + scale * ( max - min );
 }
