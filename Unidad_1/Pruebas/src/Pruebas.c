d1e8661f06da006f3fbf0c56ab9b90e6a458d2f6
Juan Cardozo <juan.cardozog@upb.edu.co>
2021-02-16 22:51:11 -0500

    Carpeta de pruebas

--- Unidad_1/Pruebas/src/Pruebas.c
+++ Unidad_1/Pruebas/src/Pruebas.c
@@ -9,46 +9,16 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-
-	int count = 0;
-	int ch;
-	FILE *in_file1;
-	FILE *in_file2;
-	char FILE_NAME1[];
-	char FILE_NAME2[];
-
-	printf("Ingrese el nombre del primer archivo: ");
-	scanf("%s\n", &FILE_NAME1);
-	printf("Ingrese el nombre del segundo archivo: ");
-	scanf("%s\n", &FILE_NAME2);
-
-	in_file1 = fopen(FILE_NAME1, "r");
-
-	if(in_file1 == NULL)
-	{
-		printf("Cannot open %s\n", FILE_NAME1);
-		exit(8);
-	}
-	in_file2 = fopen(FILE_NAME1, "w");
-
-		if(in_file2 == NULL)
-		{
-			printf("Cannot open %s\n", FILE_NAME2);
-			exit(8);
-		}
-	while(!feof(FILE_NAME1))
-	{
-		ch = fgetc(in_file1);
-		if(ch != EOF)
-			fputc(ch,FILE_NAME2);
-			++count;
-	}
-	printf("Number of characters written was %d\n", count);
-	fclose(in_file1);
-	fclose(in_file2);
-	return 0;
+    int a;
+    printf("enter a number: ");
+    while (scanf("%d", &a) != 1)
+    {
+        // input was not a number, ask again:
+        printf("enter a number: ");
+    }
+    printf("You entered %d.\n", a);
 }
+
